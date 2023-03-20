3619d87d67dc3613bc108e3342f4020ed85a4ca1
Mohit Kumar Indoriya <111172771+MohitIndoriya@users.noreply.github.com>
2023-03-20 12:55:37 +0530

    Update db.connect.js

--- Backend/src/db/db.connect.js
+++ Backend/src/db/db.connect.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose")
 mongoose.set('strictQuery', false);
 let connect = async () => {
-    return mongoose.connect(process.env.MONGOURL)
+    return await mongoose.connect(process.env.MONGOURL)
 }
 
 module.exports = connect
