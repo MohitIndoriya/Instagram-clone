f1826b48772ff79e326f69d94bec3c892925c046
MohitIndoriya <mkibap@gmail.com>
2023-03-20 08:21:17 +0530

    backend added

---
+++ Backend/src/routes/post.routes.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const { ADDNEWPOST, GETALLPOSTS, LIKEPOST, COMMENTONAPOST } = require("../controllers/post.controllers")
+const authMiddleware = require("../middlewares/auth.middleware")
+const app = express.Router()
+
+app.post("/newpost", authMiddleware, ADDNEWPOST)
+app.get("/allposts", authMiddleware, GETALLPOSTS)
+app.patch("/like/:postid", authMiddleware, LIKEPOST)
+app.post("/comment/:postid", authMiddleware, COMMENTONAPOST)
+
+
+module.exports = app 
