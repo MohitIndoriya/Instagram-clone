5b13799469d2ce6a2c1c2d5cc6d9a3e91966d123
Mohit Kumar Indoriya <111172771+MohitIndoriya@users.noreply.github.com>
2023-03-20 11:46:50 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,13 +1,13 @@
 require("dotenv").config()
 const express = require('express');
 const cors = require('cors');
-const http = require('http');
-const socketio = require('socket.io');
+// const http = require('http');
+// const socketio = require('socket.io');
 
 const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
-const messageModel = require("./models/message.model");
+// const server = http.createServer(app);
+// const io = socketio(server);
+// const messageModel = require("./models/message.model");
 const connect = require("./db/db.connect");
 
 const userRoute=require("./routes/user.routes")
@@ -19,39 +19,39 @@ app.use(cors());
 app.use("/user",userRoute)
 app.use("/post",postRoute)
 // Handle socket.io connections
-const activeusers=new Map()
-io.on('connection', (socket) => {
-  console.log('Socket connected:', activeusers);
-  let userid=socket.request._query.userid
-  activeusers.set(userid,socket.id)
-  io.emit("user_connected",userid)
-  socket.on('newMessage', async ({ sender, recipient, message }) => {
-    try {
-      // const newMessage = new messageModel({ sender, recipient, message });
-      // await newMessage.save();
-      console.log("yes")
-      recipient=activeusers.get(recipient)
-      socket.to(recipient).emit('newMessage', newMessage);
-    } catch (err) {
-      console.error(err);
-    }
-  });
-  socket.on('likePost', (postId) => {
-    // Handle likePost event here
-  });
+// const activeusers=new Map()
+// io.on('connection', (socket) => {
+//   console.log('Socket connected:', activeusers);
+//   let userid=socket.request._query.userid
+//   activeusers.set(userid,socket.id)
+//   io.emit("user_connected",userid)
+//   socket.on('newMessage', async ({ sender, recipient, message }) => {
+//     try {
+//       // const newMessage = new messageModel({ sender, recipient, message });
+//       // await newMessage.save();
+//       console.log("yes")
+//       recipient=activeusers.get(recipient)
+//       socket.to(recipient).emit('newMessage', newMessage);
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   });
+//   socket.on('likePost', (postId) => {
+//     // Handle likePost event here
+//   });
 
-  // Disconnect socket
-  socket.on('disconnect', () => {
-    console.log('Socket disconnected:', socket.id);
-    let userid=socket.request._query.userid
-    activeusers.delete(userid)
-  io.emit("user_disconnected",userid)
-  });
-});
+//   // Disconnect socket
+//   socket.on('disconnect', () => {
+//     console.log('Socket disconnected:', socket.id);
+//     let userid=socket.request._query.userid
+//     activeusers.delete(userid)
+//   io.emit("user_disconnected",userid)
+//   });
+// });
 
 // Start server
 const PORT = process.env.PORT || 8080;
-server.listen(PORT, async() => {
+app.listen(PORT, async() => {
     await connect()
     console.log(`Server started on port ${PORT}`)
 });
